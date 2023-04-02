8f8a01a3c550eac6859930a89d381c2c2958fcc7
Amanda Longo <125140476+aclongo@users.noreply.github.com>
2023-04-02 14:46:36 -0400

    Update following-links.py

--- following-links.py
+++ following-links.py
@@ -1,5 +1,9 @@
 # ASSIGNMENT: extract the href values from the anchor tags, 
 # scan for a tag that is in a particular position relative to the first link in the list, follow that link and repeat the process a number of times
+# Start at: http://py4e-data.dr-chuck.net/known_by_Banan.html
+# Find the link at position 18 (the first name is 1). Follow that link. 
+# Repeat this process 7 times. The answer is the last name that you retrieve.
+# Hint: The first character of the name of the last page that you will load is: Z
 
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
