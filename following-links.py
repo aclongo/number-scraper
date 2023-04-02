7ed9cd5b5b145f390444e48ba7c18c9b76020919
aclongo <125140476+aclongo@users.noreply.github.com>
2023-04-02 13:32:52 -0400

    Comment assignment instructions with sample

--- following-links.py
+++ following-links.py
@@ -1,3 +1,11 @@
+# ASSIGNMENT: extract the href= vaues from the anchor tags, 
+# scan for a tag that is in a particular position relative to the first name in the list, 
+# follow that link and repeat the process a number of times and report the last name you find
+    # SAMPLE: Start at http://py4e-data.dr-chuck.net/known_by_Fikret.html
+    # Find the link at position 3 (the first name is 1). Follow that link. Repeat this process 4 times. The answer is the last name that you retrieve.
+    # Sequence of names: Fikret Montgomery Mhairade Butchi Anayah
+    # Last name in sequence: Anayah
+
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 import ssl
