24e2a3141a5577f71723838d7b8d18f0d3036b9d
aclongo <125140476+aclongo@users.noreply.github.com>
2023-04-02 14:22:45 -0400

    Use on assignment url after successful sample test

--- following-links.py
+++ following-links.py
@@ -1,10 +1,9 @@
 # ASSIGNMENT: extract the href= vaues from the anchor tags, 
 # scan for a tag that is in a particular position relative to the first name in the list, 
 # follow that link and repeat the process a number of times and report the last name you find
-    # Sample problem: Start at http://py4e-data.dr-chuck.net/known_by_Fikret.html
-    # Find the link at position 3 (the first name is 1). Follow that link. Repeat this process 4 times. The answer is the last name that you retrieve.
-    # Sequence of names: Fikret Montgomery Mhairade Butchi Anayah
-    # Last name in sequence: Anayah
+    # Start at: http://py4e-data.dr-chuck.net/known_by_Banan.html
+    # Find the link at position 18 (the first name is 1). Follow that link. Repeat this process 7 times. The answer is the last name that you retrieve.
+    # Hint: The first character of the name of the last page that you will load is: Z
 
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
