8da75b21927749ef41fb5d134889c3feb4712697
aclongo <125140476+aclongo@users.noreply.github.com>
2023-04-02 13:18:46 -0400

    Clean comments

--- number-scraper.py
+++ number-scraper.py
@@ -1,6 +1,5 @@
-# The program will use urllib to read the HTML from the data files below, 
-# and parse the data, extracting numbers 
-# and compute the sum of the numbers in the file.
+# ASSIGNMENT: find all the <span> tags in the html file pull out the 
+# text content of the span tag, convert them to integers and add them up
     # Sample data: http://py4e-data.dr-chuck.net/comments_42.html (Sum=2553)
     # Actual data: http://py4e-data.dr-chuck.net/comments_1768582.html 
         # (Sum ends with 45)
@@ -14,17 +13,16 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
+# Prompt user to enter the url of a webpage
 url = input('Enter - ')
+# Use urllib to read the html
 html = urllib.request.urlopen(url, context=ctx).read()
+# Use BeautifulSoup to parse and clean up the html
 soup = BeautifulSoup(html, 'html.parser')
-
-# You are to find all the <span> tags in the file 
-# and pull out the text content of the span tag, 
-# convert them to integers and add them up
-# <tr><td>Modu</td><td><span class="comments">90</span></td></tr>
 
 # Initialize a sum total variable
 sum = 0
+# Find all span tags in the new soup variable
 span_tags = soup('span')
 for tag in span_tags:
     # Pull out contents of span tag
